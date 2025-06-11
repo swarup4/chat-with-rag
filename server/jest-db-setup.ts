738c246d7a7a6af299c4f050806d08155d70b916
Swarup Saha <swarup.saha004@hotmail.com>
2025-06-12 02:03:00 +0530

    Update jest-db-setup.ts

--- server/jest-db-setup.ts
+++ server/jest-db-setup.ts
@@ -2,18 +2,17 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();               // make .env values available in tests
+dotenv.config();
 let mongo: MongoMemoryServer;
 
 beforeAll(async () => {
     mongo = await MongoMemoryServer.create();
     await mongoose.connect(mongo.getUri());
-    process.env.SECRET_KEY = 'test-secret';   // JWT needs some key
-    jest.setTimeout(20_000);                  // raise global timeout to 20 s
+    process.env.SECRET_KEY = 'test-secret';
+    jest.setTimeout(20000);
 });
 
 afterEach(async () => {
-    // wipe all collections between tests
     if (mongoose.connection.db) {
         const collections = await mongoose.connection.db.collections();
         for (const c of collections) await c.deleteMany({});
